81a8190754014da1349434f233f4e994f08ba5ee
sjo1403 <64970935+sjo1403@users.noreply.github.com>
2021-07-14 22:58:25 -0400

    Truck status can now be examined

--- Packages.py
+++ Packages.py
@@ -1,5 +1,8 @@
+import Trucks
+
+
 class Package:
-    def __init__(self, ID, address, city, state, zip, deadline, mass, truck):
+    def __init__(self, ID, address, city, state, zip, deadline, mass, truck, status="AT-HUB"):
         self.ID = ID
         self.address = address
         self.city = city
@@ -8,26 +11,29 @@ class Package:
         self.deadline = deadline
         self.mass = mass
         self.truck = truck
-
-        def ID(self):
-            print(ID)
-        def address(self):
-            print(address)
-        def city(self):
-            print(city)
-        def state(self):
-            print(state)
-        def zip(self):
-            print(zip)
-        def deadline(self):
-            print(deadline)
-        def mass(self):
-            print(mass)
-        def truck(self):
-            print(truck)
+        self.status = status
 
 def info(Package):
     info = "Package ID: " + str(Package.ID) + "\t" + str(Package.address) + "\t" + str(Package.city) + ", " + \
            str(Package.state) + "\t" + str(Package.zip) + "\tDELIVER BY: " + str(Package.deadline) + "\t" \
-           + str(Package.mass) + " pounds"
+           + str(Package.mass) + " pounds\t" + str(Package.status)
     print(info)
+
+def checkStatus(currentTime):
+    for truck in Trucks.trucks:
+
+        timeElapsed = (currentTime - truck.start)
+        currentMile = (timeElapsed * 0.3)
+
+        if currentMile <= 0:
+            continue
+
+        else:
+            sum = 0
+            index = 0
+            while sum < currentMile:
+                sum += truck.distance[index]
+                index += 1
+
+            currentLocation = truck.route[index - 1]
+            print(str(truck.ID) + ": " + str(currentLocation))
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import HashTable
 import Packages
-
-#read data from packages file
-import csv
-
 import Trucks
+
+# read data from packages file
+import csv
 
 data = open("packageFile.csv", 'r')
 rawData = csv.reader(data)
@@ -16,12 +15,11 @@ for line in rawData:
         trim = string.strip().split('\t')
         packageData.append(trim)
 
-#create HashTable instance and insert packages
-
+# create HashTable instance and insert packages
 packages = HashTable.HashTable()
 
-i = 1   #int i used for key in HashTable
-for line in range(1,41):    #create Package objects from packageFile data
+i = 1  # int i used for key in HashTable
+for line in range(1, 41):  # create Package objects from packageFile data
     packageID = packageData[line][0]
     address = packageData[line][1]
     city = packageData[line][2]
@@ -31,8 +29,19 @@ for line in range(1,41):    #create Package objects from packageFile data
     mass = packageData[line][6]
     truck = packageData[line][7]
 
-    Package = Packages.Package(packageID,address,city,state,zip,deadline,mass,truck)
+    Package = Packages.Package(packageID, address, city, state, zip, deadline, mass, truck)
     packages.hashInsert(i, Package)
-    Trucks.loadPachages(Package)
+    Trucks.loadPackages(Package)
 
     i += 1
+
+truck1 = Trucks.Truck("Truck 1", Trucks.T1Packages, 0, Trucks.T1Route, Trucks.T1Dist)
+truck2 = Trucks.Truck("Truck 2", Trucks.T2Packages, 0, Trucks.T2Route, Trucks.T2Dist)
+truck11 = Trucks.Truck("Truck 1", Trucks.T11Packages, 90, Trucks.T11Route, Trucks.T11Dist)
+truck22 = Trucks.Truck("Truck 2", Trucks.T22Packages, 65, Trucks.T22Route, Trucks.T22Dist)
+Trucks.trucks.append(truck1)
+Trucks.trucks.append(truck2)
+Trucks.trucks.append(truck11)
+Trucks.trucks.append(truck22)
+
+Packages.checkStatus(42)
